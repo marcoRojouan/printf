049483079dc147c434c8cd6695cb2d2729053340
Marco Rojouan <mrojouan@3C3.42angouleme.fr>
2025-10-25 17:22:27 +0200

    fix : empty function removed

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mrojouan <mrojouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 13:33:42 by mrojouan          #+#    #+#             */
-/*   Updated: 2025/10/25 16:08:52 by mrojouan         ###   ########.fr       */
+/*   Updated: 2025/10/25 17:22:07 by mrojouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,8 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-// Core
 int		ft_printf(const char *format, ...);
 
-// Print functions
 int		ft_print_char(va_list list);
 int		ft_print_str(va_list list);
 int		ft_print_int(va_list list);
